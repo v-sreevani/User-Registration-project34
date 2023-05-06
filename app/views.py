82f6bf94b0980f5d611f0b6f4a6559798d579e8e
v-sreevani <vaddisreevani1234@gmail.com>
2023-05-06 16:00:53 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from app.forms import *
 from django.http import HttpResponse
 from django.core.mail import send_mail
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate,login,logout
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def registration(request):
@@ -29,7 +33,37 @@ def registration(request):
 
 
             return HttpResponse('registration successfully')
+
         else:
             return HttpResponse('not valid')
 
     return render(request,'registration.html',d)
+
+def home(request):
+
+    if request.session.get('username'):
+        username=request.session.get('username')
+        d={'username':username}
+        return render(request,'home.html',d)
+
+    return render(request,'home.html')
+
+def user_login(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+
+        AUO=authenticate(username=username,password=password)
+        if AUO and AUO.is_active:
+            login(request,AUO)
+            request.session['username']=username
+            return HttpResponseRedirect(reverse('home'))
+        else:
+            return HttpResponse('Invalid username or password') 
+
+    return render(request,'user_login.html')
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('home'))
